d85134cfde20c1fdae7f78b706f8f271679cea46
Siddhant <forsomethingnewsid@gmail.com>
2025-03-13 17:44:10 +0000

    commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+
+pip install -r requirements.txt
+wget -P checkpoint https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz
+wget https://download.pytorch.org/tutorial/hymenoptera_data.zip
+unzip hymenoptera_data.zip
+rm hymenoptera_data.zip
